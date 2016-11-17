9a45587aa5cdd6a10cad2486430d7ba5764b4d59
mitch <mitchelltaylor@gmail.com>
2016-11-16 20:22:24 -0500

    add app6

---
+++ app6/ui.R
@@ -0,0 +1,19 @@
+library(shiny)
+shinyUI(fluidPage(
+    titlePanel("Tabs!"),
+    sidebarLayout(
+        sidebarPanel(
+            textInput("box1", "Enter Tab 1 Text:", value = "Tab 1!"),
+            textInput("box2", "Enter Tab 2 Text:", value = "Tab 2!"),
+            textInput("box3", "Enter Tab 3 Text:", value = "Tab 3!")
+        ),
+        mainPanel(
+            # tab panel works like main and sidebar panel functions
+            tabsetPanel(type = "tabs", 
+                        tabPanel("Tab 1", br(), textOutput("out1")), 
+                        tabPanel("Tab 2", br(), textOutput("out2")), 
+                        tabPanel("Tab 2", br(), textOutput("out3"))
+            )
+        )
+    )
+))

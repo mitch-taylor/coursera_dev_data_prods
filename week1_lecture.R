872bbb055bcd996394f7eeaaef9afcd986bf73ae
Taylor <MT84249@AD1.PROD>
2016-11-22 13:07:35 -0500

    Update notes for half of Shiny gadgets

--- week1_lecture.R
+++ week1_lecture.R
@@ -7,3 +7,28 @@
 # runApp() will run the shiny app, can also use runApp(path)
 # all standard html tags exist in shiny
 # ?builder
+
+### SHINY GADGETS
+
+# shiny gadgets is more for interactivity in R studio than outward facing to customers
+# launches small single page app in the viewer panel
+
+library(shiny)
+library(miniUI)
+
+myFirstGadget <- function() {
+    ui <- miniPage( # like ui.R
+        gadgetTitleBar("My First Gadget")
+    )
+    server <- function(input, output, session) { # like server.R
+        # The Done button closes the app
+        observeEvent(input$done, {
+            stopApp()
+        })
+    }
+    runGadget(ui, server)
+}
+
+myFirstGadget() 
+
+

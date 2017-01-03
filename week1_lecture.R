96ec0168caec11f3c41a57b3c60865ac231cc0c6
Mitch T <mitchelltaylor@gmail.com>
2017-01-03 12:19:41 -0500

    update week1 lecture notes

--- week1_lecture.R
+++ week1_lecture.R
@@ -16,6 +16,7 @@
 library(shiny)
 library(miniUI)
 
+# gadget 1
 myFirstGadget <- function() {
     ui <- miniPage( # like ui.R
         gadgetTitleBar("My First Gadget")
@@ -31,4 +32,57 @@ myFirstGadget <- function() {
 
 myFirstGadget() 
 
+# gadget 2
+multiplyNumbers <- function(numbers1, numbers2) {
+    ui <- miniPage(
+        gadgetTitleBar("Multiply Two Numbers"),
+        miniContentPanel( # main body of the pain
+            selectInput("num1", "First Number", choices=numbers1), # drag down box
+            selectInput("num2", "Second Number", choices=numbers2) # NB choices comes from function
+        )
+    )
+    server <- function(input, output, session) {
+        observeEvent(input$done, { # care with {}
+            num1 <- as.numeric(input$num1) # create num1/2 so easier to work with
+            num2 <- as.numeric(input$num2)
+            stopApp(num1 * num2)
+        })
+    }
+    runGadget(ui, server) # need to have this
+}
+
+multiplyNumbers(1:10, 1:10)
+
+# gadget 3
+pickTrees <- function() {
+    ui <- miniPage(
+        gadgetTitleBar("Select Points by Dragging your Mouse"),
+        miniContentPanel(
+            plotOutput("plot", height = "100%", brush = "brush") # name of plot is plot, brush is brush
+        )
+    )
+    server <- function(input, output, session) {
+        output$plot <- renderPlot({ # note {}
+            plot(trees$Girth, trees$Volume, main = "Trees!",
+                 xlab = "Girth", ylab = "Volume")
+        })
+        observeEvent(input$done, {
+            stopApp(brushedPoints(trees, input$brush, # stop after collecting brushed points
+                                  xvar = "Girth", yvar = "Volume"))
+        })
+    }
+    runGadget(ui, server)
+}
+
+trees_picked <- pickTrees()
+trees_picked
+
+### GOOGLEVIS
+
+# Connects R to googles visualisation API
+
+
+    
+    
+    
 

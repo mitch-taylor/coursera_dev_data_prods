2af5fef77b4ef2bce18bd16061b23e35da89d5e7
mitch <mitchelltaylor@gmail.com>
2017-01-15 20:30:00 -0500

    finish wk 1 lecture

--- week1_lecture.R
+++ week1_lecture.R
@@ -79,10 +79,116 @@ trees_picked
 
 ### GOOGLEVIS
 
-# Connects R to googles visualisation API
+# Connects R to googles visualisation API - interactive graphics
 
+# example - note geared to embed chart in html document
+# motion chart
+suppressPackageStartupMessages(library(googleVis))
+M <- gvisMotionChart(Fruits, "Fruit", "Year",
+                     options=list(width=600, height=400))
+plot(M)
+print(M) # print relevant html page / javascript
 
-    
-    
-    
+# plots on maps
+G <- gvisGeoChart(Exports, locationvar="Country",
+                  colorvar="Profit",options=list(width=600, height=400))
+plot(G)
 
+# specifying a region
+G2 <- gvisGeoChart(Exports, locationvar="Country",
+                   colorvar="Profit",options=list(width=600, height=400,region="150"))
+plot(G2)
+    
+# setting more options
+df <- data.frame(label=c("US", "GB", "BR"), val1=c(1,3,4), val2=c(23,12,32))
+Line <- gvisLineChart(df, xvar="label", yvar=c("val1","val2"),
+                      options=list(title="Hello World", legend="bottom",
+                                   titleTextStyle="{color:'red', fontSize:18}",                         
+                                   vAxis="{gridlines:{color:'red', count:3}}",
+                                   hAxis="{title:'My Label', titleTextStyle:{color:'blue'}}",
+                                   series="[{color:'green', targetAxisIndex: 0}, 
+                                   {color: 'blue',targetAxisIndex:1}]",
+                                   vAxes="[{title:'Value 1 (%)', format:'##,######%'}, 
+                                   {title:'Value 2 (\U00A3)'}]",                          
+                                   curveType="function", width=500, height=300                         
+                      ))
+plot(Line)
+
+# combining plots
+G <- gvisGeoChart(Exports, "Country", "Profit",options=list(width=200, height=100))
+T1 <- gvisTable(Exports,options=list(width=200, height=270))
+M <- gvisMotionChart(Fruits, "Fruit", "Year", options=list(width=400, height=370))
+GT <- gvisMerge(G,T1, horizontal=FALSE)
+GTM <- gvisMerge(GT, M, horizontal=TRUE,tableOptions="bgcolor=\"#CCCCCC\" cellspacing=10")
+plot(GTM)
+
+# in R markdown documents set results = asis for it to work
+
+### PLOTLY
+
+# plotly is a web application for creating and sharing visualisations - works
+# on R, matlab, python, etc.
+library(plotly)
+plot_ly(mtcars, x = ~wt, y = ~mpg, mode = "markers") # markers = scatter
+# note interactive graphic is knitted into html
+plot_ly(mtcars, x = ~wt, y = ~mpg, mode = "markers", color = ~factor(cyl))
+# use cylinder to colour
+plot_ly(mtcars, x = ~wt, y = ~mpg, mode = "markers", color = ~disp)
+# colour and size based on variables
+plot_ly(mtcars, x = ~wt, y = ~mpg, mode = "markers", 
+        color = ~factor(cyl), size = ~hp)
+
+# 3D scatter plot
+set.seed(2016-07-21)
+temp <- rnorm(100, mean = 30, sd = 5)
+pressue <- rnorm(100)
+dtime <- 1:100
+plot_ly(x = ~temp, y = ~pressue, z = ~dtime,
+        type = "scatter3d", color = ~temp)
+
+# line graph (timeseries)
+data("airmiles")
+plot_ly(x = ~time(airmiles), y = ~airmiles, type = "scatter", mode = "lines")
+
+library(tidyr);library(dplyr)
+data("EuStockMarkets")
+
+stocks <- as.data.frame(EuStockMarkets) %>% # convert time series to df
+    gather(index, price) %>%
+    mutate(time = rep(time(EuStockMarkets), 4))
+# note how we had to change data structure to be long rather than wide
+# this was done with gather (tidyr)
+plot_ly(stocks, x = ~time, y = ~price, color = ~index, type = "scatter", mode = "lines")
+
+# histogram
+plot_ly(x = ~precip, type = "histogram")
+# boxplot
+plot_ly(iris, y = ~Petal.Length, color = ~Species, type = "box")
+# heatmap
+terrain1 <- matrix(rnorm(100*100), nrow = 100, ncol = 100)
+plot_ly(z = ~terrain1, type = "heatmap") # z is a matrix
+# surface
+terrain2 <- matrix(sort(rnorm(100*100)), nrow = 100, ncol = 100)
+plot_ly(z = ~terrain2, type = "surface") # z is intensity, x and y are given
+
+#Choropleth maps
+# Create data frame
+state_pop <- data.frame(State = state.abb, Pop = as.vector(state.x77[,1]))
+# Create hover text
+state_pop$hover <- with(state_pop, paste(State, '<br>', "Population:", Pop))
+# Make state borders white
+borders <- list(color = toRGB("red"))
+# Set up some mapping options
+map_options <- list(
+    scope = 'usa',
+    projection = list(type = 'albers usa'),
+    showlakes = TRUE,
+    lakecolor = toRGB('white')
+)
+
+plot_ly(z = ~state_pop$Pop, text = ~state_pop$hover, locations = ~state_pop$State, 
+        type = 'choropleth', locationmode = 'USA-states', 
+        color = state_pop$Pop, colors = 'Blues', marker = list(line = borders)) %>%
+    layout(title = 'US Population in 1975', geo = map_options)
+
+# Note can convert ggplot2 to plotly

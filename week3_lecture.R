fdb137abbea1b528c61deba7a63077a63617d112
mitch <mitchelltaylor@gmail.com>
2017-01-23 22:21:39 -0500

    continuing wk3 notes

--- week3_lecture.R
+++ week3_lecture.R
@@ -22,9 +22,32 @@
 # written in specific markup language
 
 # building the package R CMD build creates the package archive file
-# R CMD check runs a battery of tests, can run both from command line. 
+# R CMD check runs a battery of tests, can run both from command line.
 # Can also run from R using the system function system("R CMD build newpackage)
 
 # package.skeleton() creates the skeleton directory structory for a package
+
+### CLASSES & METHODS
+
+# developing data types / structures that R doesn't by default support
+# classes and methods basically for object oriented programming - a little different
+# than other programming languages in it's implementation
+# Two styles S3/S4. S3 is old style, less formal and less rigorous.
+# code for developing S4 classes and methods is in methods package
+# class defined by setClass(). Objects are instances and can be created with new()
+# methods are functions that operate on certain classes. A generic function
+# dispatches the methods and doesn't do any actual computation.
+# every object in R has a class and can be determined with class()
+# lm function creates a lm class
+# to see methods associated with a generic function use methods()/showMethods()
+methods(mean) #S3
+showMethods(show) #s4
+# to view code for a method need getS3method() or getMethod()
+head(getS3method("mean", "default"), 10)
+# rule is to never call methods directly
+
+
+
+
 
 
